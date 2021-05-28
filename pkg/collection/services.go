6300087e0d02c8a2da302e82e7427f721ec041fa
Markus Frosch <markus.frosch@netways.de>
2021-05-28 13:54:12 +0200

    services: Add Find functions

--- pkg/collection/services.go
+++ pkg/collection/services.go
@@ -2,7 +2,10 @@ package collection
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 const (
@@ -10,11 +13,19 @@ const (
 	ServiceManagerSysV    = "sysv"
 )
 
+const sysVInitD = "/etc/init.d"
+
 // ErrNoServiceManager is returned when we could not detect one.
 var ErrNoServiceManager = errors.New("could not detect a supported service manager")
 
 // FoundServiceManager remembers the current service manager found.
 var FoundServiceManager string
+
+var systemdUnitPaths = []string{
+	"/etc/systemd/system",
+	"/usr/lib/systemd/system",
+	"/lib/systemd/system",
+}
 
 func DetectServiceManager() string {
 	if FoundServiceManager != "" {
@@ -45,4 +56,57 @@ func GetServiceStatusRaw(name string) ([]byte, error) {
 	default:
 		return []byte{}, ErrNoServiceManager
 	}
+}
+
+func FindServices(pattern string) (map[string]string, error) {
+	switch DetectServiceManager() {
+	case ServiceManagerSystemD:
+		return FindServicesSystemd(pattern)
+	case ServiceManagerSysV:
+		return FindServicesSysV(pattern)
+	default:
+		return nil, ErrNoServiceManager
+	}
+}
+
+func FindServicesSystemd(pattern string) (map[string]string, error) {
+	units := map[string]string{}
+
+	for _, path := range systemdUnitPaths {
+		files, err := filepath.Glob(filepath.Join(path, pattern))
+		if err != nil {
+			return nil, fmt.Errorf("could not glob with pattern '%s': %w", pattern, err)
+		}
+
+		for _, file := range files {
+			// Skip the file if it is a symlink
+			if stat, err := os.Lstat(file); err != nil || (stat.Mode()&os.ModeSymlink) == os.ModeSymlink {
+				continue
+			}
+
+			name := filepath.Base(file)
+
+			// Only safe the first found unit
+			if _, ok := units[name]; !ok {
+				units[name] = file
+			}
+		}
+	}
+
+	return units, nil
+}
+
+func FindServicesSysV(pattern string) (map[string]string, error) {
+	units := map[string]string{}
+
+	files, err := filepath.Glob(filepath.Join(sysVInitD, pattern))
+	if err != nil {
+		return nil, fmt.Errorf("could not glob with pattern '%s': %w", pattern, err)
+	}
+
+	for _, file := range files {
+		units[filepath.Base(file)] = file
+	}
+
+	return units, nil
 }
