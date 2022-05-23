796cb0a869c34f485ef13817c358989dd886f1ad
tbauriedel <tobias.bauriedel@netways.de>
2022-05-23 13:16:03 +0200

    add timestamp for journalctl

--- modules/icingaweb2/collector.go
+++ modules/icingaweb2/collector.go
@@ -116,7 +116,7 @@ func Collect(c *collection.Collection) {
 
 	for name, element := range journalctlLogs {
 		if service, err := collection.FindServices(element.Service); err == nil && len(service) > 0 {
-			c.AddCommandOutput(ModuleName+"/"+name, "journalctl", "-u", element.Service)
+			c.AddCommandOutput(ModuleName+"/"+name, "journalctl", "-u", element.Service, "--since \"7 days ago\"")
 		}
 	}
 
