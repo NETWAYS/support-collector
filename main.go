fe17acb1bc9138dabec0cb7b0567529a6e8ad1fe
Markus Opolka <markus.opolka@netways.de>
2023-04-04 14:22:05 +0200

    Add Elastic Stack collector

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/NETWAYS/support-collector/modules/ansible"
 	"github.com/NETWAYS/support-collector/modules/base"
 	"github.com/NETWAYS/support-collector/modules/corosync"
+	"github.com/NETWAYS/support-collector/modules/elastic"
 	"github.com/NETWAYS/support-collector/modules/grafana"
 	"github.com/NETWAYS/support-collector/modules/graphite"
 	"github.com/NETWAYS/support-collector/modules/graylog"
@@ -53,6 +54,7 @@ var modules = map[string]func(*collection.Collection){
 	"icinga2":         icinga2.Collect,
 	"icingaweb2":      icingaweb2.Collect,
 	"icinga-director": icingadirector.Collect,
+	"elastic":         elastic.Collect,
 	"corosync":        corosync.Collect,
 	"keepalived":      keepalived.Collect,
 	"mysql":           mysql.Collect,
@@ -73,6 +75,7 @@ var (
 		"icingaweb2",
 		"icinga-director",
 		"icingadb",
+		"elastic",
 		"corosync",
 		"keepalived",
 		"mysql",
